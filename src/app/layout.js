19d126ad2ac70608df69ee1443a8468329301d0a
ubaid ur rehman <programming112233@gmail.com>
2025-05-20 22:31:14 +0500

    fixed no index issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,35 @@
+// import "./globals.css";
+// import { ClerkProvider } from "@clerk/nextjs";
+
+// export const metadata = {
+//   title: "GranaME",
+//   description: "GranaME is a platform for learning and teaching languages.",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <ClerkProvider publishableKey="pk_test_bGVhZGluZy1zcGFycm93LTcyLmNsZXJrLmFjY291bnRzLmRldiQ">
+//         <body>{children}</body>
+//       </ClerkProvider>
+//     </html>
+//   );
+// }
+
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata = {
   title: "GranaME",
   description: "GranaME is a platform for learning and teaching languages.",
+  metadataBase: new URL("https://granameapp.com"),
+  alternates: {
+    canonical: "/",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({ children }) {
